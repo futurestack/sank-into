f8f7afb5912dc87b7bd0dcd42b2cf8ed96b1e78e
Michael Patraw <mpatraw@ubuntu.(none)>
2010-08-12 12:16:49 -0700

    Added the quad tree.

--- src/quadtree.h
+++ src/quadtree.h
@@ -1,94 +1,158 @@
-/*
- *  quadtree.h
- *  sank-into
- *
- *  Created by hornet on 8/4/10.
- *  Copyright 2010 __MyCompanyName__. All rights reserved.
- *
- */
-
-#ifndef QUADTREE_H_
-#define QUADTREE_H_
+///
+/// Quad tree implementation.
+///
+#ifndef QUADTREE_H
+#define QUADTREE_H
 
 #include <cmath>
 #include <cstddef>
 
-#include <vector>
-
 template <typename T>
 class QuadTree
+{
+public:
+    // Helper constants.
+    enum
     {
-    public:
-        static const size_t SIZE_1x1 =      0;
-        static const size_t SIZE_2x2 =      1;
-        static const size_t SIZE_4x4 =      2;
-        static const size_t SIZE_8x8 =      3;
-        static const size_t SIZE_16x16 =    4;
-        static const size_t SIZE_32x32 =    5;
-        static const size_t SIZE_64x64 =    6;
-        static const size_t SIZE_128x128 =  7;
-        static const size_t SIZE_256x256 =  8;
-        static const size_t SIZE_512x512 =  9;
-        static const size_t SIZE_1024x1024 =10;
-        
-        enum Sector
-        {
-            TOP_LEFT, TOP_RIGHT, BOT_LEFT, BOT_RIGHT
-        };
-        
-        /// Constructs a quadtree with a width and height of 2^layers.
-        QuadTree(size_t layers);
-        virtual ~QuadTree();
-        
-        virtual size_t getWidth() const;
-        virtual size_t getHeight() const;
-        
-        /// Sets an object for this tree.
-        virtual void setObject(T data);
-        /// Gets the object for this tree.
-        virtual T &getObject();
-        
-        /// Sets the object or every sub quadtree with this object if not
-        /// at base.
-        virtual void setObjects(T data);
-        
-        /// Returns a list of all the objects in the quadtree.
-        virtual void getObjects(std::vector<T> &vec) const;
-        
-        /// Returns a sub-quadtree of this tree, where l is the layer of the tree
-        /// to index. Layer of 0 gives you a 1x1 tree, which can only be index
-        /// by 0,0. Layer of 1 gives you a 2x2 vision of this tree.
-        virtual QuadTree<T> &getQuadTree(size_t l, size_t x, size_t y);
-        
-        // Functions for layering.
-        virtual size_t getLayers() const;
-        
-        /// Gets the element at the x,y position.
-        virtual T &operator ()(size_t x, size_t y);
-        /// Gets one of the four sub quadtrees.
-        virtual QuadTree<T> &operator ()(Sector sec);
-    protected:
-        QuadTree<T> *m_topLeft;
-        QuadTree<T> *m_topRight;
-        QuadTree<T> *m_botRight;
-        QuadTree<T> *m_botLeft;
-        T m_data;
-        size_t m_layers;
-        size_t m_curLayer;
+        SIZE_1x1 =     0, SIZE_2x2 =        1, SIZE_4x4 =       2,
+        SIZE_8x8 =     3, SIZE_16x16 =      4, SIZE_32x32 =     5,
+        SIZE_64x64 =   6, SIZE_128x128 =    7, SIZE_256x256 =   8,
+        SIZE_512x512 = 9, SIZE_1024x1024 = 10, SIZE_2048x2048 = 11
     };
+
+    /// Constructs a quadtree with a width and height of 2^layers.
+    QuadTree(size_t layers=0);
+    virtual ~QuadTree();
+
+    virtual void clear();
+    
+    /// Gets the number of layers this tree has.
+    virtual size_t layers() const { return m_layers; }
+    
+    /// Gets the width of the "current layer". See the operator []
+    /// for details.
+    virtual size_t width() const;
+    
+    /// Gets the height of the "current layer". See the operator []
+    /// for details.
+    virtual size_t height() const;
+    
+    /// Returns true if this tree has a parent. A parent always
+    /// has four children.
+    virtual bool hasParent() const { return m_parent != NULL; }
+    
+    /// Returns the parent. Segfaults if this tree doesn't have one. :(
+    virtual QuadTree<T> &parent() { return *m_parent; }
+
+    /// Sets this tree's object, and EVERY child's object to this.
+    virtual void setAllObjects(T to);
+    
+    /// Returns a reference the templated object.
+    virtual T &object() { return m_data; }
+    
+    /// Returns the tree at the X and Y position on the "current layer". See
+    /// the operator [] for details.
+    virtual QuadTree<T> &operator ()(size_t x, size_t y);
+
+    
+    
+    /// Sets the current layer to index from. Useful for getting any element
+    /// you desire in the whole tree.
+    ///
+    /// This operator effects the quad tree class in the following ways:
+    /// * width() and height() function returns the width and height
+    ///   (respectively) of the current layer. For example:
+    ///   `tree[0].width()` will return 1 ALWAYS :P
+    /// * The () operator indexes at that current layer. For example:
+    ///   `tree[0](0, 0)` will return just `tree` and
+    ///   `tree[1](<0 or 1>, <0 or 1).parent()` will return just `tree`
+    ///
+    /// Note: It's a VERY good idea to ALWAYS use this before calling
+    ///       width(), height and () so you know for sure you're where
+    ///       you want to be.
+    virtual QuadTree<T> &operator [](int layer);
+
+    
+    
+protected:
+    // It's a friggin forest.
+    QuadTree<T> *m_parent;
+    QuadTree<T> *m_topLeft;
+    QuadTree<T> *m_topRight;
+    QuadTree<T> *m_botRight;
+    QuadTree<T> *m_botLeft;
+    
+    // The dada. Eh-hem, I mean data.
+    T m_data;
+    
+    // The number of layers indexable.
+    size_t m_layers;
+    // The current layer we're indexing.
+    size_t m_curLayer;
+
+    // Do we have the right to free them? :O
+    bool m_owner;
+    
+private:
+    // Private function to set parents n' stuff.
+    QuadTree(QuadTree<T> *parent, size_t layers);
+
+    // Copying *Soon to come*
+    QuadTree(const QuadTree<T> &qt);
+    QuadTree<T> &operator =(const QuadTree<T> &qt);
+};
 
 template <typename T>
 QuadTree<T>::QuadTree(size_t layers)
 {
     m_layers = layers;
     m_curLayer = layers;
+    // An orphan. :'(
+    m_parent = NULL;
+    
+    m_owner = true;
     
     if (m_layers > 0)
     {
-        m_topLeft = new QuadTree<T>(m_layers - 1);
-        m_topRight = new QuadTree<T>(m_layers - 1);
-        m_botLeft = new QuadTree<T>(m_layers - 1);
-        m_botRight = new QuadTree<T>(m_layers - 1);
+        // Use the private constructor so we can supply a parent.
+        m_topLeft = new QuadTree<T>(this, m_layers - 1);
+        m_topRight = new QuadTree<T>(this, m_layers - 1);
+        m_botLeft = new QuadTree<T>(this, m_layers - 1);
+        m_botRight = new QuadTree<T>(this, m_layers - 1);
+    }
+    else
+    {
+        m_topLeft = NULL;
+        m_topRight = NULL;
+        m_botLeft = NULL;
+        m_botRight = NULL;
+    }
+}
+
+// PRIVATE, GO AWAY
+template <typename T>
+QuadTree<T>::QuadTree(const QuadTree<T> &qt)
+{
+    *this = qt;
+}
+
+template <typename T>
+QuadTree<T>::QuadTree(QuadTree<T> *parent, size_t layers)
+{
+    m_layers = layers;
+    m_curLayer = layers;
+    // Yay!
+    m_parent = parent;
+
+    m_owner = true;
+    
+    if (m_layers > 0)
+    {
+        // printf("slow\n");                                       jk
+        m_topLeft = new QuadTree<T>(this, m_layers - 1);
+        m_topRight = new QuadTree<T>(this, m_layers - 1);
+        m_botLeft = new QuadTree<T>(this, m_layers - 1);
+        m_botRight = new QuadTree<T>(this, m_layers - 1);
     }
     else
     {
@@ -102,281 +166,135 @@ QuadTree<T>::QuadTree(size_t layers)
 template <typename T>
 QuadTree<T>::~QuadTree()
 {
-    if (m_topLeft && m_topRight && m_botLeft && m_botRight)
+    clear();
+}
+
+template <typename T>
+void QuadTree<T>::clear()
+{
+    if (m_layers > 0 && m_owner)
     {
         delete m_topLeft;
+        m_topLeft = NULL;
         delete m_topRight;
+        m_topRight = NULL;
         delete m_botLeft;
+        m_botLeft = NULL;
         delete m_botRight;
+        m_botRight = NULL;
     }
 }
 
 template <typename T>
-size_t QuadTree<T>::getWidth() const
+size_t QuadTree<T>::width() const
 {
-    return static_cast<size_t>(pow(2.0f, (float)m_layers));
+    return static_cast<size_t>(pow(2.0f, (float)m_curLayer));
 }
 
 template <typename T>
-size_t QuadTree<T>::getHeight() const
+size_t QuadTree<T>::height() const
 {
-    return static_cast<size_t>(pow(2.0f, (float)m_layers));
+    return static_cast<size_t>(pow(2.0f, (float)m_curLayer));
 }
 
 template <typename T>
-void QuadTree<T>::setObject(T data)
+void QuadTree<T>::setAllObjects(T to)
 {
-    m_data = data;
-}
-
-template <typename T>
-T &QuadTree<T>::getObject()
-{
-    return m_data;
-}
-
-template <typename T>
-void QuadTree<T>::setObjects(T data)
-{
-    if (m_topLeft && m_topRight && m_botLeft && m_botRight)
-    {
-        m_topLeft->setObjects(data);
-        m_topRight->setObjects(data);
-        m_botLeft->setObjects(data);
-        m_botRight->setObjects(data);
-        m_data = data;
-    }
-    else
-    {
-        m_data = data;
-    }
-}
-
-template <typename T>
-void QuadTree<T>::getObjects(std::vector<T> &vec) const
-{
-    if (m_topLeft && m_topRight && m_botLeft && m_botRight)
-    {
-        m_topLeft->getObjects(vec);
-        m_topRight->getObjects(vec);
-        m_botLeft->getObjects(vec);
-        m_botRight->getObjects(vec);
-    }
-    else
-    {
-        vec.push_back(m_data);
-    }
-}
-
-template <typename T>
-QuadTree<T> &QuadTree<T>::getQuadTree(size_t d, size_t x, size_t y)
-{
-    size_t width = static_cast<size_t>(pow(2.0f, (float)d));
-    size_t height = static_cast<size_t>(pow(2.0f, (float)d));
+    this->object() = to;
     
-    if (d > 0)
+    // Children?
+    if (m_layers > 0)
     {
+        m_topLeft->setAllObjects(to);
+        m_topRight->setAllObjects(to);
+        m_botLeft->setAllObjects(to);
+        m_botRight->setAllObjects(to);
+    }
+}
+
+template <typename T>
+QuadTree<T> &QuadTree<T>::operator ()(size_t x, size_t y)
+{
+    size_t d = m_curLayer;
+    
+    QuadTree<T> *ptr = this;
+    
+    // Might be possible to implement this recursively. Where might means
+    // definitely.
+    while (d > 0)
+    {
+        size_t width = static_cast<size_t>(pow(2.0f, (float)d));
+        size_t height = static_cast<size_t>(pow(2.0f, (float)d));
+        
         if (x < width / 2 && y < height / 2)
         {
-            return m_topLeft->getQuadTree(d - 1, x, y);
+            ptr = ptr->m_topLeft;
         }
         else if (x < width && y < height / 2)
         {
-            return m_topRight->getQuadTree(d - 1, x - width / 2, y);
+            x -= width / 2;
+            ptr = ptr->m_topRight;
         }
         else if (x < width / 2 && y < height)
         {
-            return m_botLeft->getQuadTree(d - 1, x, y - height / 2);
+            y -= height / 2;
+            ptr = ptr->m_botLeft;
         }
         else
         {
-            return m_botRight->getQuadTree(d - 1, x - width / 2, y - height / 2);
+            x -= width / 2;
+            y -= height / 2;
+            ptr = ptr->m_botRight;
         }
+
+        d -= 1;
     }
+    
+    // Segfault? :D
+    return *ptr;
+}
+
+template <typename T>
+QuadTree<T> &QuadTree<T>::operator [](int layer)
+{
+    // Python indexing.
+    if (layer < 0)
+        m_curLayer = m_layers + (layer + 1);
     else
-    {
-        return (*this);
-    }
+        m_curLayer = layer;
+    return *this;
 }
 
+// PRIVATE, GO AWAY
 template <typename T>
-size_t QuadTree<T>::getLayers() const
+QuadTree<T> &QuadTree<T>::operator =(const QuadTree<T> &qt)
 {
-    return m_layers;
-}
-
-template <typename T>
-T &QuadTree<T>::operator ()(size_t x, size_t y)
-{
-    return getQuadTree(m_layers, x, y).getObject();
-}
-
-template <typename T>
-QuadTree<T> &QuadTree<T>::operator ()(Sector sec)
-{
-    switch (sec)
+    if (this != &qt)
     {
-        case TOP_LEFT:
-            return (*m_topLeft);
-        case TOP_RIGHT:
-            return (*m_topRight);
-        case BOT_LEFT:
-            return (*m_botLeft);
-        case BOT_RIGHT:
-            return (*m_botRight);
-    }
-}
+        clear();
+        m_layers = qt.m_layers;
+        m_curLayer = qt.m_curLayer;
+        m_parent = qt.m_parent;
+        m_owner = false;
+        m_data = qt.m_data;
 
-#endif /* QUADTREE_H_ */
-
-
-#include <SDL/SDL.h>
-#include <iostream>
-
-using std::cout;
-using std::endl;
-
-const int WIDTH = 640;
-const int HEIGHT = 480;
-
-class BlockWorld : public QuadTree<bool>
-{
-public:
-    BlockWorld() :
-    QuadTree<bool>(QuadTree<bool>::SIZE_32x32)
-    {
-    }
-    virtual ~BlockWorld() { }
-    
-    virtual void Draw()
-    {
-        Draw(*this, 0, 0, WIDTH, HEIGHT);
-    }
-    
-    virtual void Update()
-    {
-        
-    }
-    
-    virtual void Click(int x, int y)
-    {
-        int layerWidth = pow(2, getLayers());
-        int layerHeight = pow(2, getLayers());
-        int blockx = x / (WIDTH / layerWidth);
-        int blocky = y / (HEIGHT / layerHeight);
-        (*this)(blockx, blocky) = true;
-    }
-    
-    virtual void Key(unsigned int num, int x, int y)
-    {
-        if (num > getLayers())
-            return;
-        int layerWidth = pow(2, num);
-        int layerHeight = pow(2, num);
-        int blockx = x / (WIDTH / layerWidth);
-        int blocky = y / (HEIGHT / layerHeight);
-        getQuadTree(num, blockx, blocky).setObjects(false);
-        getQuadTree(num, blockx, blocky).setObject(true);
-    }
-    
-    virtual void BreakUp(int x, int y)
-    {
-        unsigned int layers = 1;
-        int blockx = x, blocky = y;
-        
-        while (layers < getLayers())
+        if (m_layers != 0)
         {
-            int layerWidth = pow(2, layers);
-            int layerHeight = pow(2, layers);
-            blockx = x / (WIDTH / layerWidth);
-            blocky = y / (HEIGHT / layerHeight);
-            
-            if (getQuadTree(layers, blockx, blocky).getObject() == true)
-            {
-                getQuadTree(layers, blockx, blocky).setObjects(false);
-                getQuadTree(layers, blockx, blocky)(TOP_LEFT).setObject(true);
-                getQuadTree(layers, blockx, blocky)(TOP_RIGHT).setObject(true);
-                getQuadTree(layers, blockx, blocky)(BOT_LEFT).setObject(true);
-                getQuadTree(layers, blockx, blocky)(BOT_RIGHT).setObject(true);
-                return;
-            }
-            else
-            {
-                layers += 1;
-            }
-        }
-    }
-protected:
-    
-    virtual void Draw(QuadTree<bool> &bw, int x, int y, int w, int h)
-    {
-        if (bw.getObject() == false)
-        {
-            if (bw.getLayers() == 0)
-                return;
-            Draw(bw(TOP_LEFT),  x,         y,         w / 2, h / 2);
-            Draw(bw(TOP_RIGHT), x + w / 2, y,         w / 2, h / 2);
-            Draw(bw(BOT_LEFT),  x,         y + h / 2, w / 2, h / 2);
-            Draw(bw(BOT_RIGHT), x + w / 2, y + h / 2, w / 2, h / 2);
+            m_topLeft = qt.m_topLeft;
+            m_topRight = qt.m_topRight;
+            m_botLeft = qt.m_botLeft;
+            m_botRight = qt.m_botRight;
         }
         else
         {
-            SDL_Rect r;
-            r.x = x + 1;
-            r.y = y + 1;
-            r.w = w - 1;
-            r.h = h - 1;
-            SDL_FillRect(SDL_GetVideoSurface(), &r, 0x00CCAA77);
+            m_topLeft = NULL;
+            m_topRight = NULL;
+            m_botLeft = NULL;
+            m_botRight = NULL;
         }
     }
-};
-
-/*
-int main()
-{
-    bool running = true;
-    SDL_Event ev;
-    BlockWorld bw;
-    
-    SDL_SetVideoMode(WIDTH, HEIGHT, 32, SDL_DOUBLEBUF);
-    
-    while (running)
-    {
-        SDL_FillRect(SDL_GetVideoSurface(), NULL, 0);
-        bw.Draw();
-        bw.Update();
-        SDL_Flip(SDL_GetVideoSurface());
-        
-        while (SDL_PollEvent(&ev))
-        {
-            switch (ev.type)
-            {
-                case SDL_QUIT:
-                    running = false;
-                    break;
-                case SDL_MOUSEBUTTONDOWN:
-                    bw.Click(ev.motion.x, ev.motion.y);
-                    break;
-                case SDL_KEYDOWN:
-                {
-                    int x, y;
-                    SDL_GetMouseState(&x, &y);
-                    if (ev.key.keysym.sym == SDLK_c)
-                        bw.setObjects(false);
-                    if (ev.key.keysym.sym == SDLK_s)
-                        bw.BreakUp(x, y);
-                    else
-                        bw.Key(ev.key.keysym.sym - '0', x, y);
-                    break;
-                }
-                default:
-                    break;
-            }
-        }
-    }
-    
-    SDL_Quit();
-    return 0;
+    return *this;
 }
 
-*/
+#endif /* QUADTREE_H */
+
